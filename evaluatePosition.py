b84de1019ae78e149a2d49a4130ade1008f61faf
Eliah Trosien <124410616+EliahTrosien@users.noreply.github.com>
2025-06-15 16:45:07 +0200

    Update evaluatePosition.py

--- evaluatePosition.py
+++ evaluatePosition.py
@@ -5,7 +5,8 @@ def getEvaluation(game_state):
     healthHeuristic = heuristics.OwnHealth()
     spaceHeuristic = heuristics.SpaceSimple()
     distanceHeuristic = heuristics.DistanceOfHeads()
-    #headAreaHeuristic = heuristics.AreaAroundHead()
+    headAreaHeuristic = heuristics.AreaAroundHead()
     return (0.2 * healthHeuristic.normalizedHeuristic(game_state) +
-            0.4 * spaceHeuristic.normalizedHeuristic(game_state) +
-            0.4 * distanceHeuristic.normalizedHeuristic(game_state))
+            0.3 * spaceHeuristic.normalizedHeuristic(game_state) +
+            0.3 * distanceHeuristic.normalizedHeuristic(game_state) + 
+            0.2 * headAreaHeuristic.normalizedHeuristic(game_state))
